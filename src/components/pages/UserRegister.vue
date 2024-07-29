81a3f198fdcb5ecd4d46ae386653aa2455436b93
Yasminakhtari <akhtariy85@gmail.com>
2024-07-29 15:11:39 +0530

    validatuion added in register page

--- src/components/pages/UserRegister.vue
+++ src/components/pages/UserRegister.vue
@@ -3,77 +3,93 @@
     <div class="container mt-4">
       <div class="row justify-content-center">
         <div class="col-md-8 border rounded shadow p-4" :style="{ backgroundColor: '#e8e8b3' }">
-          <form action="">
+          <form @submit.prevent="userRegister">
             <h3 class="text-center m-5">User Registration Form</h3>
             <div class="row">
               <div class="col-md-6">
-
                 <div class="input-field">
-                  <label htmlFor="firstName">First Name</label>
+                  <label for="firstName">First Name</label>
                   <input v-model="project.firstName" type="text" class="form-control" id="firstName" name="firstName" />
+                  <div v-if="showErrors && !v$.project.firstName.$pending && !v$.project.firstName.$model"
+                    class="text-danger">
+                    First Name is required.
+                  </div>
                 </div>
 
                 <div class="input-field">
-                  <label htmlFor="lastName">Last Name</label>
+                  <label for="lastName">Last Name</label>
                   <input v-model="project.lastName" type="text" class="form-control" id="lastName" name="lastName" />
+                  <div v-if="showErrors && !v$.project.lastName.$pending && !v$.project.lastName.$model"
+                    class="text-danger">
+                    Last Name is required.
+                  </div>
                 </div>
               </div>
-              <div class="col-md-6">
 
+              <div class="col-md-6">
                 <div class="input-field">
-                  <label htmlFor="companyName">Company Name</label>
+                  <label for="companyName">Company Name</label>
                   <input v-model="project.companyName" type="text" class="form-control" id="companyName"
                     name="companyName" />
+                  <div v-if="showErrors && !v$.project.companyName.$pending && !v$.project.companyName.$model"
+                    class="text-danger">
+                    Company Name is required.
+                  </div>
                 </div>
                 <div class="input-field">
-                  <label htmlFor="phoneNumber">Phone Number</label>
+                  <label for="phoneNumber">Phone Number</label>
                   <input v-model="project.phoneNumber" type="text" class="form-control" id="phoneNumber"
                     name="phoneNumber" />
+                  <div v-if="showErrors && !v$.project.phoneNumber.$pending && !v$.project.phoneNumber.$model"
+                    class="text-danger">
+                    Phone Number is required.
+                  </div>
                 </div>
               </div>
-              <div class="col-md-6">
 
-                <label htmlFor="emailId">EmailId</label>
+              <div class="col-md-6">
                 <div class="input-field">
+                  <label for="emailId">EmailId</label>
                   <input v-model="project.emailId" type="text" class="form-control" id="emailId" name="emailId" />
+                  <div v-if="showErrors && !v$.project.emailId.$pending && !v$.project.emailId.$model"
+                    class="text-danger">
+                    EmailId is required.
+                  </div>
                 </div>
 
                 <div class="input-field">
-                  <label htmlFor="address">Address</label>
+                  <label for="address">Address</label>
                   <input v-model="project.address" type="text" class="form-control" id="address" name="address" />
+                  <div v-if="showErrors && !v$.project.address.$pending && !v$.project.address.$model"
+                    class="text-danger">
+                    Address is required.
+                  </div>
                 </div>
               </div>
 
-
               <div class="col-md-6">
-
                 <div class="input-field">
                   <label for="password">Password</label>
-                  <div class="input-group">
-                    <input v-model="project.password" class="form-control" id="password" name="password"
-                       />
-                       <!-- :type="showPassword ? 'text' : 'password'" -->
-                    <!-- <button class="btn btn-secondary" type="button" @click="togglePasswordVisibility">
-                      <i :class="showPassword ? 'bi bi-eye' : 'bi bi-eye-slash'"></i>
-                    </button> -->
+                  <input v-model="project.password" class="form-control" id="password" name="password" type="password"/>
+                  <div v-if="showErrors && !v$.project.password.$pending && !v$.project.password.$model"
+                    class="text-danger">
+                    Password is required.
                   </div>
                 </div>
                 <div class="input-field">
-                  <label htmlFor="confirmPassword">Confirm Password</label>
-                  <div class="input-group">
-                    <input v-model="project.confirmPassword" class="form-control" id="confirmPassword"
-                      name="confirmPassword"  />
-<!-- :type="user.showConfirmPassword ? 'text' : 'confirmPassword'" -->
-                    <!-- <button class="btn btn-secondary" type="button" @click="togglePasswordVisibility">
-                      <i :class="showPassword ? 'bi bi-eye' : 'bi bi-eye-slash'"></i>
-                    </button> -->
+                  <label for="confirmPassword">Confirm Password</label>
+                  <input v-model="project.confirmPassword" class="form-control" id="confirmPassword"
+                    name="confirmPassword" type="password"/>
+                  <div v-if="showErrors && !v$.project.confirmPassword.$pending && !v$.project.confirmPassword.$model"
+                    class="text-danger">
+                    Confirm Password is required.
                   </div>
                 </div>
               </div>
-              <div class="d-grid gap-2 col-6 mx-auto m-5">
 
+              <div class="d-grid gap-2 col-6 mx-auto m-5">
                 <div class="text-center">
-                  <button type="submit" class="btn btn-primary" @click="userRegister()" :disabled="isSaving">
+                  <button type="submit" class="btn btn-primary" :disabled="isSaving">
                     Register
                   </button>
                 </div>
@@ -90,77 +106,97 @@
     </div>
   </layout-div>
 </template>
+
 <script>
 import LayoutDiv from '../LayoutDiv.vue';
 import axios from 'axios';
-import Swal from 'sweetalert2'
+import Swal from 'sweetalert2';
+import { reactive, ref } from 'vue';
+import { required } from '@vuelidate/validators';
+import useVuelidate from '@vuelidate/core';
+import { useRouter } from 'vue-router';
 
 export default {
   name: 'UserRegister',
   components: {
     LayoutDiv,
   },
-  data() {
-    return {
+  setup() {
+    const project = reactive({
+      firstName: '',
+      lastName: '',
+      companyName: '',
+      phoneNumber: '',
+      emailId: '',
+      address: '',
+      password: '',
+      confirmPassword: ''
+    });
+
+    const showErrors = ref(false);
+    const isSaving = ref(false);
+    const router = useRouter(); // Use the router instance
+
+    const rules = {
       project: {
-        firstName: '',
-        lastName: '',
-        companyName: '',
-        phoneNumber: '',
-        emailId: '',
-        address: '',
-        password: '',
-        confirmPassword: '',
-        userName: ''
-      },
-      // user: {
-        showPassword: false,
-        // showConfirmPassword: false,
-      // },
-      isSaving: false,
+        firstName: { required },
+        lastName: { required },
+        companyName: { required },
+        phoneNumber: { required },
+        emailId: { required },
+        address: { required },
+        password: { required },
+        confirmPassword: { required }
+      }
     };
-  },
-  methods: {
-    userRegister() {
-      this.project.userName = this.project.emailId;
-      this.isSaving = true
-      axios.post('/api/saveUserDetails', this.project)
-        .then(response => {
-          Swal.fire({
-            icon: 'success',
-            title: 'Register successfully!',
-            showConfirmButton: true,
-            timer: 1500
-          })
-          this.isSaving = false
-          this.project.firstName = ""
-          this.project.lastName = ""
-          this.project.companyName = ""
-          this.project.address = ""
-          this.project.phoneNumber = ""
-          this.project.password = ""
-          this.project.confirmPassword
-          return response
-        })
-        .catch(error => {
-          this.isSaving = false
-          Swal.fire({
-            icon: 'error',
-            title: 'An Error Occured!',
-            showConfirmButton: true,
-            timer: 1500
-          }).then(() => {
-            this.$router.push('/');
-          })
-          return error
+
+    const v$ = useVuelidate(rules, { project });
+
+    const userRegister = async () => {
+      showErrors.value = true;
+      v$.value.$touch();
+      if (v$.value.$invalid) {
+        console.log('Validation failed:', v$.value);
+        return;
+      }
+      project.userName = project.emailId;
+      isSaving.value = true;
+      try {
+        const response = await axios.post('/api/saveUserDetails', project); // Use project directly
+        Swal.fire({
+          icon: 'success',
+          title: 'Register successfully!',
+          showConfirmButton: true,
+          timer: 1500
         });
-    },
-    togglePasswordVisibility() {
-      this.showPassword = !this.showPassword;
-    },
-    // passwordVisibility() {
-    //   this.showConfirmPassword = !this.showConfirmPassword;
-    // }
-  },
-}
+        // Reset form
+        Object.keys(project).forEach(key => {
+          project[key] = '';
+        });
+        isSaving.value = false;
+        router.push('/'); // Use router for redirection
+        console.log('Registration response:', response);
+      } catch (error) {
+        isSaving.value = false;
+        Swal.fire({
+          icon: 'error',
+          title: 'An Error Occurred!',
+          showConfirmButton: true,
+          timer: 1500
+        }).then(() => {
+          router.push('/'); // Use router for redirection
+        });
+        console.log('Registration error:', error);
+      }
+    };
+
+    return {
+      project,
+      v$,
+      showErrors,
+      userRegister,
+      isSaving
+    };
+  }
+};
 </script>
