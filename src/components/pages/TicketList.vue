a89596b31f8c597222d71b2e82f68da49f895a3a
bibhu nayak <bibhu.nayak@westechltd.com>
2024-07-31 16:43:40 +0530

    update done

--- src/components/pages/TicketList.vue
+++ src/components/pages/TicketList.vue
@@ -58,9 +58,12 @@
                                 <td>
                                     <!-- <router-link :to="`/show/${ticket.id}`"
                                         class="btn btn-outline-info mx-1">Show</router-link> -->
-                                    <router-link>
+                                    <!-- <router-link>
                                         <i class="bi bi-pencil-fill text-primary mx-2"></i>
-                                    </router-link>
+                                    </router-link> -->
+                                    <a data-bs-toggle="modal" data-bs-target="#exampleModal" @click="openModal(ticket)">
+                                        <i class="bi bi-pencil-fill text-primary mx-2"></i>
+                                    </a>
                                     <i class="bi bi-trash3-fill text-danger" @click="handleDelete(ticket.id)"></i>
                                 </td>
                             </tr>
@@ -69,13 +72,184 @@
                 </div>
             </div>
         </div>
+
+
+<!--Modal-->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-lg">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Ticket Details</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <form  @submit.prevent="update">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="field1" class="form-label">Company Name</label>
+                                        <input type="text" class="form-control" id="field1" v-model="selectedTicket.companyName"/>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="field2" class="form-label">Address</label>
+                                        <input type="text" class="form-control" id="field2" v-model="selectedTicket.address"
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="field1" class="form-label">Person Name</label>
+                                        <input type="text" class="form-control" id="field1" v-model="selectedTicket.personName"
+                                         />
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="field2" class="form-label">Phone Number</label>
+                                        <input type="tel" id="phone" class="form-control"
+                                            v-model="selectedTicket.phoneNumber"
+                                             />
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="field1" class="form-label">Email Id</label>
+                                        <input type="text" class="form-control" id="field1" v-model="selectedTicket.emailId"
+                                         />
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="field2" class="form-label">Delivery Date</label>
+                                        <input type="date" class="form-control" id="field2"
+                                        v-model="selectedTicket.expectedDeliveryDate"/>
+                                    </div>
+                                </div>
+
+                                <div class="col-md-4">
+                                    <div class="form-group">
+                                        <label for="commentBox" class="form-label">User Status</label>
+                                        <input type="text" class="form-control" v-model="selectedTicket.status" />
+                                    </div>
+                                </div>
+                                <!-- <div class="col-md-4">
+                                    <div class="form-group">
+                                        <label for="createdOn" class="form-label">User Submission Date</label>
+                                        <input type="text" :value="formattedUserSubmissionDate" class="form-control"  />
+                                    </div>
+                                </div> -->
+                                <div class="col-md-4">
+                                    <div class="form-group">
+                                        <label for="createdOn" class="form-label">Ticket Type</label>
+                                        <input type="text" v-model="selectedTicket.ticketName" class="form-control" />
+                                    </div>
+                                </div>
+                                <div class="col-md-3">
+                                    <div class="form-group">
+                                        <label for="createdOn" class="form-label">Priority Type</label>
+                                        <input type="text" v-model="selectedTicket.priorityName" class="form-control"
+                                        disabled  readonly />
+                                    </div>
+                                </div>
+                                <div class="col-md-3">
+                                    <div class="form-group">
+                                        <label for="commentBox" class="form-label">User Comment</label>
+                                        <textarea rows="1" cols="20" id="commentBox" v-model="selectedTicket.commentBox"
+                                            class="form-control" placeholder="Enter your comment here..."
+                                            ></textarea>
+                                    </div>
+                                </div>
+                                <div class="col-md-3">
+                                    <div class="form-group">
+                                        <label for="fileView" class="form-label">User File</label>
+                                        <div class="file-item">
+                                            <button class="btn btn-success btn-sm" @click.prevent="downloadFile(selectedTicket.fileId)" target="_blank">
+                                                <i class="fas fa-file-pdf mx-1"></i>Download
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-3" v-if="activeTab === 'GENERATED'">
+                                    <div class="form-group">
+                                        <label for="fileView" class="form-label">Resolved File</label>
+                                        <div class="file-item">
+                                            <button class="btn btn-success btn-sm" target="_blank" @click.prevent="downloadResolvedFile(selectedTicket.resolvedFileId)">
+                                                <i class="fas fa-file-pdf mx-1"></i>Download
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                <button type="submit" class="btn btn-primary">Save</button>
+                            </div>
+                            </div>
+                        </form>
+                        <!-- <form @submit.prevent="adminAssigned">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <label for="status">Status</label>
+                                    <select class="form-select" v-model="status">
+                                        <option value="">--select--</option>
+                                        <option value="ASSIGNED" v-if="activeTab === 'SUBMITTED'">ASSIGN</option>
+                                        <option value="APPROVED" v-if="activeTab === 'GENERATED'">APPROVE</option>
+                                        <option value="REJECTED">REJECT</option>
+
+                                    </select>
+                                </div>
+                                <div class="col-md-6" v-if="activeTab === 'SUBMITTED'">
+                                    <label>Employee Name</label>
+                                    <select class="form-select" v-model="assignedTo">
+                                        <option value="">--select--</option>
+                                        <option v-for="data in listOfEmployee" :key="data.id" :value="data.id">
+                                            {{ data.firstName }} {{ data.lastName }}
+                                        </option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="heading-container">
+                                <h4>Comment Section</h4>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <label>Admin Comment</label>
+                                    <textarea class="form-control" v-model="comment">
+
+                                    </textarea>
+
+                                </div>
+
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                <button type="submit" class="btn btn-primary">Save</button>
+                            </div>
+                        </form> -->
+                        
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+
+
+
     </layout-div>
 </template>
 
 <script>
 import axios from 'axios';
 import LayoutDiv from '../LayoutDiv.vue';
-import Swal from 'sweetalert2'
+import Swal from 'sweetalert2';
+import { Modal } from 'bootstrap';
+
 export default {
     name: 'TicketList',
     components: {
@@ -94,6 +268,22 @@ export default {
                 { name: 'REJECTED', label: 'REJECTED' },
                 { name: 'CANCELLED', label: 'CANCELLED' },
             ],
+            // selectedTicket: {
+            //     companyName:'',
+            //     address: '',
+            //     personName: '',
+            //     phoneNumber: '',
+            //     emailId: '',
+            //     expectedDeliveryDate: '',
+            //     status: '',
+            //     createdOn: '',
+            //     ticketName: '',
+            //     priorityName: '',
+            //     commentBox: '',
+            //     requestFormCode:''
+            // },
+             selectedTicket: {},
+
             allStatistic: {}
         };
     },
@@ -141,6 +331,46 @@ export default {
                     return error
                 });
         },
+
+        openModal(ticket) {
+            this.selectedTicket = ticket;
+            this.fetchTicketList();
+            // this.fetchEmployeeList();
+        },
+
+        update() {
+            console.log(this.selectedTicket)
+            axios.put(`/api/updateRequestFormData`, this.selectedTicket)
+                .then(response => {
+                    console.log(response);
+                    this.fetchTicketList();
+                    Swal.fire({
+                        icon: 'success',
+                        title: 'Updated',
+                        text: 'The ticket was updated successfully',
+                    }).then(() => {
+                        const modalElement = document.getElementById('exampleModal');
+                        const modalInstance = Modal.getInstance(modalElement);
+                        modalInstance.hide();
+                        const backdrop = document.querySelector('.modal-backdrop');
+                        if (backdrop) {
+                            backdrop.parentNode.removeChild(backdrop);
+                        }
+                        this.fetchTicketList();
+                    });
+                    
+                })
+                .catch(error => {
+                    console.log(error);
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Error',
+                        text: 'There was a problem updating the ticket',
+                    });
+                });
+        },
+
+
         formatDate(date) {
             const d = new Date(date);
             const day = String(d.getDate()).padStart(2, '0');
